7191ba269ae74b6e2b35d4c1f001cbe279b6b2fc
Erich-Reitz <alohae.reitz@gmail.com>
2021-12-03 12:10:11 -0600

    rrd

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Home } from './pages/Home'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+<BrowserRouter>
+      <div className="container">
+        <Switch>
+          <Route path="/" component={Home} exact />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
