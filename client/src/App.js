b23123264bceeaf7e0151c3cf9d2b252d4758abd
Erich-Reitz <alohae.reitz@gmail.com>
2021-12-03 13:02:51 -0600

    homejs

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Home } from './pages/Home'
+import Home from './pages/Home.js'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import './Home.css';
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                    </p>
+            </div>
+            
+        );
+    }
+
+}
+
+export default Home;
